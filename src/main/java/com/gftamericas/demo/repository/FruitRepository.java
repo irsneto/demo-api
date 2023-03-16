4592795cc0efcb59cb8b17158d54811607fa74d9
iort <inacio.neto@gft.com>
2023-03-16 14:30:07 -0300

    Add repository

---
+++ src/main/java/com/gftamericas/demo/repository/FruitRepository.java
@@ -0,0 +1,9 @@
+package com.gftamericas.demo.repository;
+
+import com.gftamericas.demo.model.Fruit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FruitRepository extends JpaRepository<Long, Fruit> {
+}
