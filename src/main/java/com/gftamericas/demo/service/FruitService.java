a5311107cf4fb0202ff155c4aa74356fa4addec5
iort <inacio.neto@gft.com>
2023-03-16 14:44:51 -0300

    Add service

---
+++ src/main/java/com/gftamericas/demo/service/FruitService.java
@@ -0,0 +1,24 @@
+package com.gftamericas.demo.service;
+
+import com.gftamericas.demo.model.Fruit;
+import com.gftamericas.demo.repository.FruitRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class FruitService {
+
+    private FruitRepository fruitRepository;
+
+    public Fruit createFruit(Fruit fruit) {
+        return fruitRepository.save(fruit);
+    }
+
+    public List<Fruit> getAll(){
+        List<Fruit> fruits = fruitRepository.findAll();
+        return fruits;
+    }
+}
